84ca4c7a4e0dd06d7996a7e2be308191cd75dd9e
Hamed Ghasemzadeh <evgnomon@gmail.com>
2015-12-17 13:18:31 +0330

    settings for tsse

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:12.04
+FROM ubuntu:14.04
 MAINTAINER Francesco Sullo, sullof@sullof.com, http://sullof.com
 
 ENV DEBIAN_FRONTEND noninteractive
--- adds/configure.sh
+++ adds/configure.sh
@@ -3,9 +3,17 @@
 mkdir /var/run/sshd
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
-mv /authorized_keys /root/.ssh/.
-chmod 600 /root/.ssh/*
+#mv /authorized_keys /root/.ssh/.
+#chmod 600 /root/.ssh/*
 chown -Rf root:root /root/.ssh
+
+
+useradd -m france
+mkdir -p /home/france/.ssh
+chmod 700 /home/france/.ssh
+mv /authorized_keys /home/france/.ssh/.
+chmod 600 /home/france/.ssh/*
+chown -Rf france:france /home/france/.ssh
 
 # configure sshd to block authentication via password
 sed -i.bak 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
